4e927d1b6c97c2f9fe2abf5856b3b230618642a3
Martin 'Arkardiusz' Mlostek <mlostek@gmail.com>
2019-06-18 14:21:12 +0200

    swift5 fix merged https://github.com/facebook/react-native/commit/df7ea67#diff-6dc01074330d7af8d99288cad5a021b7R129

--- .vscode/.history/React/Base/RCTBridgeModule_20190618142022.h
+++ .vscode/.history/React/Base/RCTBridgeModule_20190618142022.h
@@ -211,18 +211,6 @@ RCT_EXTERN void RCTRegisterModule(Class); \
   - (returnType)method RCT_DYNAMIC;
 
 /**
- * Same as RCT_EXPORT_MODULE, but uses __attribute__((constructor)) for module
- * registration. Useful for registering swift classes that forbids use of load
- * Used in RCT_EXTERN_REMAP_MODULE
- */
-#define RCT_EXPORT_MODULE_NO_LOAD(js_name, objc_name) \
-RCT_EXTERN void RCTRegisterModule(Class); \
-+ (NSString *)moduleName { return @#js_name; } \
-__attribute__((constructor)) static void \
-RCT_CONCAT(initialize_, objc_name)() { RCTRegisterModule([objc_name class]); }
-
-
-/**
  * Use this macro in a private Objective-C implementation file to automatically
  * register an external module with the bridge when it loads. This allows you to
  * register Swift or private Objective-C classes with the bridge.
@@ -262,7 +250,7 @@ RCT_CONCAT(initialize_, objc_name)() { RCTRegisterModule([objc_name class]); }
   @interface objc_name (RCTExternModule) <RCTBridgeModule> \
   @end \
   @implementation objc_name (RCTExternModule) \
-  RCT_EXPORT_MODULE_NO_LOAD(js_name, objc_name)
+  RCT_EXPORT_MODULE(js_name)
 
 /**
  * Use this macro in accordance with RCT_EXTERN_MODULE to export methods
